405d49e9f706119df4f86792b2ef8749f32d05f7
falkdanielj <falkdanielj@gmail.com>
2022-03-15 19:24:42 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("e517722a-ea9c-430d-afad-431f9c39d47e");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
